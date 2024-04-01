5f300fecf9b7f77d18d6eb83e7bf53b28fa451c5
cxinu <cxinu3099@gmail.com>
2024-04-01 20:52:06 +0530

    Join & Archive channels on giveaway event in selected channels

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import os
 import dotenv
 from telethon import TelegramClient, events
-import logging
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+from telethon.tl.types import MessageMediaGiveaway
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.custom.chatgetter import ChatGetter
 
 dotenv.load_dotenv()
-
-# Replace the values below with your own API ID and API Hash
 api_id = os.environ['API_ID']
 api_hash = os.environ['API_HASH']
-
 client = TelegramClient('jackP', api_id, api_hash)
-client.start()
 
-@client.on(events.NewMessage(chats=('me')))
+@client.on(events.NewMessage(chats=('me', -1001506456927)))
 async def handle_new_message(event):
     sender = await event.get_sender()
-    print(f"New message from {sender.username}: {event.text}")
+    if isinstance(event.message.media, MessageMediaGiveaway):
+        giveaway = event.message.media
+        
+        if not giveaway.countries_iso2 or 'IN' in giveaway.countries_iso2:
+            for channel_id in giveaway.channels:
+                channel = await client.get_input_entity(channel_id)
+                # Join chennel
+                result = await client(JoinChannelRequest(channel))
+                # Archive channel
+                await client.edit_folder(channel, 1)
+                
+                
 
+client.start()
 client.run_until_disconnected()
